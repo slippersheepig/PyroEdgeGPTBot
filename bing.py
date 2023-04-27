d6e3bd259de9170da21489083fe2625ce86396d2
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-04-27 23:37:34 +0800

    Update bing.py

--- bing.py
+++ bing.py
@@ -122,7 +122,7 @@ def callback_all(callback_query):
 async def bing_chat(message_text, message):
     if message.from_user.id not in EDGES:
         EDGES[message.from_user.id] = Chatbot(cookie_path=COOKIE_PATH)
-    bot.send_chat_action(message.chat.id, 'typing'):
+    bot.send_chat_action(message.chat.id, 'typing')
     response_dict = await EDGES[message.from_user.id].ask(prompt=message_text,
                                                           conversation_style=conversation_style)
 
